12964a3828e13664197c0d70a323dce147068604
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-23 21:27:15 +0200

    custom hooks

---
+++ src/hooks/job.ts
@@ -0,0 +1,12 @@
+import { fetchJobs, createJob } from "./../api/jobs";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+const JOBS = "JOBS";
+
+export const useJobs = () => {
+  return useQuery([JOBS], fetchJobs);
+};
+
+export const useCreateJob = () => {
+  return useMutation(createJob);
+};
