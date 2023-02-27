6652d4d207440973ee1e8aa22c5ac733776cb422
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-02-27 23:48:50 +0200

    updated Job page

--- src/pages/Jobs/Jobs.tsx
+++ src/pages/Jobs/Jobs.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import styled from "styled-components";
+import { useJobs } from "../../hooks/job";
+import JobCard from "./JobCard";
+import JobModal from "./JobModal";
 
 const Jobs = () => {
-  return <div>Jobs</div>;
+  const { data: jobs, isLoading } = useJobs();
+
+  if (isLoading) {
+    return <div>Jobs are loading...</div>;
+  }
+
+  if (!isLoading && !jobs?.length) {
+    return <div>There are no jobs added yet</div>;
+  }
+
+  return (
+    <div>
+      <Title>JOB LISTINGS</Title>
+      {jobs.map((job) => (
+        <JobCard key={job.id} job={job} />
+      ))}
+      <JobModal />
+    </div>
+  );
 };
 
 export default Jobs;
+
+const Title = styled.h1`
+  text-align: center;
+`;
